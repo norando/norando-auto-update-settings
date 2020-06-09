a5f3f7362da84f70b458c79d299460f2a60a722d
shota shinmen <info@norando.net>
2020-06-09 15:36:43 +0900

    1.1.0 themeの自動更新を追加

--- nora-auto-update-settings.php
+++ nora-auto-update-settings.php
@@ -3,9 +3,12 @@
  * Plugin Name: norando-auto-update-settings
  * Author: norando
  * Author URI: https://norando.net
- * Version: 1.0.0
+ * Version: 1.1.0
  * Description: 自動アップデートの設定
  */
 
  // プラグインの自動更新をON
 add_filter( 'auto_update_plugin', '__return_true' );
+
+// テーマの自動更新をON
+add_filter( 'auto_update_theme', '__return_true' );
